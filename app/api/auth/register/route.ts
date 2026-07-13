aad560bcbd596a7a49de631658b2729325d96b5f
yaqoobdeve <yaqoobdeve@gmail.com>
2026-07-13 11:42:55 -0700

    first commit

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,37 @@
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { request } from "http";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST() {
+    try {
+        const {email,password} = await request.json()
+        if(!email){
+            return NextResponse.json(
+                {error:"email and password are required"},
+                {status:400}
+            )
+        }
+        await connectToDatabase;
+        const existingUser = await User.findOne({email})
+        if(existingUser){
+             return NextResponse.json(
+                {error:"User already registered"},
+                {status:400}
+            )
+        }
+        await User.create({
+            email,password
+        })
+         return NextResponse.json(
+                {message:"User registered successfully"},
+                {status:200}
+            )
+    } catch (error) {
+        console.error("Registeration error")
+         return NextResponse.json(
+                {error:"Registration failed"},
+                {status:400}
+            )
+    }
+}
